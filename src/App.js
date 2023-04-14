22030600bdb7ad2db6d8f5024b7f0452da08f634
bsegelke <bbsegelke@gmail.com>
2023-04-14 18:15:27 -0400

    bundler working with typescript

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+var App = function () {
+    return (_jsx("h1", { children: " Howdy yall" }));
+};
+export default App;
+//# sourceMappingURL=App.js.map
