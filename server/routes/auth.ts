cf0e3e937059bb0337a67b9e46bd01691ddd1e5d
teddyrockwell <tmelder@xula.edu>
2023-04-15 15:20:38 -0500

    fixed login and logout

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,37 +1,50 @@
-const routes = require("express").Router();
-const passportRoute = require("passport");
-const CLIENT_URL= process.env.CLIENT_URL;
+const routes = require('express').Router();
+const passportRoute = require('passport');
+const CLIENT_URL = process.env.CLIENT_URL;
 
-routes.get("/login/success", (req:any, res:any)=>{
-  if (req.user){
+routes.get('/login/success', (req: any, res: any) => {
+  if (req.user) {
     res.status(200).json({
       success: true,
-      message: "Succesful",
+      message: 'Succesful',
       user: req.user,
-      cookies:req.cookies
-    })
+      cookies: req.cookies,
+    });
   }
+});
 
-})
-
-routes.get("/login/failed", (req:any, res:any)=>{
+routes.get('/login/failed', (req: any, res: any) => {
   res.status(401).json({
     success: false,
-    message: "failure",
-  })
+    message: 'failure',
+  });
 });
 
-routes.get("/logout", (req:any, res:any)=>{
-  req.logout();
-  res.redirect(CLIENT_URL);
+// routes.get("/logout", (req:any, res:any)=>{
+//   req.logout();
+//   res.redirect(CLIENT_URL);
+// });
+
+routes.get('/logout', (req: any, res: any) => {
+  req.logout(() => {
+    if (req.session) {
+      req.session.destroy();
+    }
+  });
+  res.redirect('/');
 });
 
-routes.get("/google", passportRoute.authenticate("google", { scope: ["profile"] }));
+routes.get(
+  '/google',
+  passportRoute.authenticate('google', { scope: ['profile'] })
+);
 
-routes.get("/google/callback", passportRoute.authenticate("google", {
+routes.get(
+  '/google/callback',
+  passportRoute.authenticate('google', {
     successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed"
-}));
+    failureRedirect: '/login/failed',
+  })
+);
 
-
-module.exports = routes
+module.exports = routes;
