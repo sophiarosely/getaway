90a9764785d0b94d323b8ac8ff0919fa1f19e26e
bsegelke <bbsegelke@gmail.com>
2023-04-15 21:28:39 -0400

    prisma succesfully creating a user table

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const dbconnection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'getaway'
-});
+// const dbconnection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '',
+//   database: 'getaway'
+// });
 
-dbconnection.connect((err:any) => {
-  if (err) {
-    console.error('Error connecting to MySQL database: ', err);
-    return;
-  }
-  console.log('Connected to MySQL database!');
-});
+// dbconnection.connect((err:any) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL database: ', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL database!');
+// });
 
 
-module.exports = dbconnection
+// module.exports = dbconnection
