e5e41db64cf5558af2b61bcda9b70e1169a23d3f
bsegelke <bbsegelke@gmail.com>
2023-04-17 15:51:42 -0400

    able to get 40 results

--- server/routes/therapist.ts
+++ server/routes/therapist.ts
@@ -1,27 +1,91 @@
 const { Router } = require("express")
+require('dotenv').config();
 const router = Router()
 const axios = require("axios")
 const GOOGLE_PLACES_API = process.env.GOOGLE_PLACES_API
 
+// router.get('/search',(req:any,res:any)=>{
+//   console.log(req.query)
+//   axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+//   params: {
+//     key: GOOGLE_PLACES_API,
+//     radius: '800000',
+//     location: `${req.query.lat},${req.query.long}`,
+//     keyword: 'therapist'
+//   }
+// })
+// .then((response:any)=>{
+// res.send(response.data).status(200)
+// })
+// .catch((err:any)=>{
+//   res.status(500)
+//   console.error("failed to get therapists", err)
+// })
+// })
+
 router.get('/search',(req:any,res:any)=>{
+  console.log(req.query)
+  let pageOne: any[] = [];
+  let pageTwo: any[] = [];
+  let pageThree: any[] = [];
   axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
-  params: {
-    key: 'AIzaSyApJBHf29dIWEv09KiG2ETirPkj8mJRNjc',
-    radius: '800000',
-    location: '29.9511,-90.0715',
-    keyword: 'therapist'
-  }
-})
-.then((response:any)=>{
-  console.log('succesful Get of therapists', response.data)
-res.send(response.data).status(200)
-})
-.catch((err:any)=>{
-  res.status(500)
-  console.error("failed to get therapists", err)
-})
+    params: {
+      key: GOOGLE_PLACES_API,
+      radius: '800000',
+      location: `${req.query.lat},${req.query.long}`,
+      keyword: 'therapist',
+      pagetoken: ''
+    }
+  })
+  .then((response:any)=>{
+    console.log(response.data.next_page_token);
+    pageOne.push(response.data.results);
+    setTimeout(()=>{
+      axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+        params: {
+          key: GOOGLE_PLACES_API,
+          radius: '800000',
+          location: `${req.query.lat},${req.query.long}`,
+          keyword: 'therapist',
+          pagetoken: response.data.next_page_token
+        }
+      })
+      .then((response:any)=>{
+        pageOne.push(response.data.results);
+        res.send(pageOne.flat()).status(200);
+      })
+      .catch((err:any)=>{
+        res.status(500);
+        console.error("failed to get therapists", err);
+      });
+    }, 2000); // wait for 2 seconds before making the second API call
+  })
+  .catch((err:any)=>{
+    res.status(500);
+    console.error("failed to get therapists", err);
+  });
 })
 
+
+
+
+
+
+router.get('/details', (req:any, res:any)=>{
+  axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+    params:{
+      place_id: "ChIJRfhonKyb5IkR2uAfRsPLrgA",
+      key: GOOGLE_PLACES_API
+    }
+  })
+  .then((response:any)=>{
+    res.send(response.data).status(200)
+  })
+  .catch((err:any)=>{
+    console.error("failed to get the details of therpaists", err);
+    res.status(500)
+  })
+})
 
 
 module.exports = router
--- src/pages/Guidance.tsx
+++ src/pages/Guidance.tsx
@@ -1,12 +1,39 @@
+import { useState, useEffect} from "react"
+import axios from "axios";
+
 const Guidance = () =>{
 
-//this function gives me the users location details
-navigator.geolocation.getCurrentPosition(position =>{
-  const { latitude, longitude } = position.coords
-  console.log(latitude, longitude)
-})
+  const [userLat, setLat] = useState(0);
+  const [userLong, setLong] = useState(0);
+  const [geolocationLoaded, setGeolocationLoaded] = useState(false);
+
+  useEffect(() => {
+    //function get the user lat and long
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      setLat(latitude);
+      setLong(longitude);
+      setGeolocationLoaded(true);
+    });
+  }, [])
+
+  useEffect(() => {
+    //if lat and long has been loaded run get request
+    if (geolocationLoaded) {
+      axios.get('/therapist/search', {
+        params:{
+          lat: userLat,
+          long: userLong
+        }
+      })
+      .then((response)=>{
+        console.log("clientside",response.data)
+      });
+    }
+  }, [geolocationLoaded, userLat, userLong])
 
 
+console.log(userLat, userLong)
 
   return (
     <h1>Guidance</h1>
