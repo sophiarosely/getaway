0a4555e2beb6067c70f75265cc2807d94c0e26e8
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-17 18:33:18 -0500

    added tracking button to card

--- src/components/Habits/Habitcard.tsx
+++ src/components/Habits/Habitcard.tsx
@@ -5,42 +5,54 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Tracking from  './Tracking'
 import axios from 'axios';
 import Box from '@mui/material/Box';
 
 interface HabitCardProps {
   id: number;
   name: string;
+  type: string
 }
 
-const HabitCard = ({ id, name }: HabitCardProps) =>{
+const HabitCard = ({ id, name, type }: HabitCardProps) =>{
 
+  const [isTrackingOpen, setIsTrackingOpen] = useState(false);
 
-
-  return (
- 
-
-<Card sx={{ backgroundColor: '#333' }}>
-         <CardContent>    
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Habit
-      </Typography>
-      <Typography variant="h5" component="div">
-      
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Type
-      </Typography>
-      <Typography variant="body2">
-        {name}
-      
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Tracking</Button>
-    </CardActions>
-    </Card>
-  )
+  const openTracking = () => {
+    setIsTrackingOpen(!isTrackingOpen);
   }
 
-  export default HabitCard;
+  const closeTracking = () => {
+    setIsTrackingOpen(false);
+  }
+
+  return (
+    <div>
+      <Card sx={{ backgroundColor: '#333' }}>
+        <CardContent>    
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Habit
+          </Typography>
+          <Typography variant="h5" component="div">
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            Habit Type: {type}
+          </Typography>
+          <Typography variant="body2">
+            {name}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={openTracking}>Tracking</Button>
+        </CardActions>
+      </Card>
+
+      {isTrackingOpen && (
+        <Tracking onClose={closeTracking} />
+      )}
+    </div>
+  )
+}
+
+export default HabitCard;
