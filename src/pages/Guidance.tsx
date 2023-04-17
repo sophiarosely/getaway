8610f1f281a8cb594e9f166e8e8780613294a688
bsegelke <bbsegelke@gmail.com>
2023-04-17 11:51:33 -0400

    ADD user location details function lat and long

--- src/pages/Guidance.tsx
+++ src/pages/Guidance.tsx
@@ -1,4 +1,13 @@
 const Guidance = () =>{
+
+//this function gives me the users location details
+navigator.geolocation.getCurrentPosition(position =>{
+  const { latitude, longitude } = position.coords
+  console.log(latitude, longitude)
+})
+
+
+
   return (
     <h1>Guidance</h1>
   )
