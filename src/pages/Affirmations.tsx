5037d26d536e83ddf6888df7a50aeee0fed2b46d
sophiarosely <pheeuh@gmail.com>
2023-04-17 16:40:08 -0500

    implementing new input field style + converting affirmation data into list

--- src/pages/Affirmations.tsx
+++ src/pages/Affirmations.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 const Affirmations = () => {
   const [userMood, setUserMood] = useState('');
-  const [affirmations, setAffirmations] = useState('');
+  const [affirmations, setAffirmations] = useState<string[]>([]);
 
   const handleSubmit = (): void => {
     axios.get(`/affirmations/mood/${userMood}`)
@@ -19,19 +19,31 @@ const Affirmations = () => {
     <center><h1>Affirmations</h1>
     <h2>What affirmations are you looking for today? </h2>
     <h3>Today, I am feeling...</h3>
-    <TextField
-    id="standard-basic"
-    label=""
-    variant="standard"
-    onChange={(e) => setUserMood(e.target.value)}
-     />
+    {/* <h6>You can express as much you want or enter a single word. It's up to you ☺ </h6> */}
+    <FormGroup>
+      <TextField
+          id="outlined-multiline-static"
+          label="Type here..."
+          multiline
+          rows={4}
+          onChange={(e) => setUserMood(e.target.value)}
+        />
      <Button
      variant="text"
      onClick={() => handleSubmit()}>
       Enter</Button>
+      </FormGroup>
     </center>
     <div id="affirmations">
-      {affirmations}
+    <ul>
+  {affirmations.map((affirmation: string, index: number) => {
+    return (
+      <li key={index}>
+        {affirmation}
+      </li>
+    );
+  })}
+</ul>
     </div>
 
     </div>
