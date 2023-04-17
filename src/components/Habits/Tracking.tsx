ef0fce16b62c892aab09cd1a533ce682aa33b535
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-17 18:33:44 -0500

    created tracking component

---
+++ src/components/Habits/Tracking.tsx
@@ -0,0 +1,26 @@
+import React,{useState, useEffect, useContext} from 'react'
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+
+
+
+const Tracking = () =>{
+
+
+
+  return (
+ 
+
+<Card sx={{ backgroundColor: '#333' }}>
+TRACKING IS OPEN
+    </Card>
+  )
+  }
+
+  export default Tracking;
