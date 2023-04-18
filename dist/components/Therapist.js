47436bcda43b6b9dbcfd8bbfdb57740a48e4b9bc
zacharydgomez <zacharydgomez@gmail.com>
2023-04-18 12:01:29 -0500

    Shouldn't exist

--- dist/components/Therapist.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-var Therapist = function (props) {
-    var therapist = props.therapist, details = props.details;
-    console.log(details);
-    return (_jsx("li", { children: therapist.name }));
-};
-export default Therapist;
-//# sourceMappingURL=Therapist.js.map
