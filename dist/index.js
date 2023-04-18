d5121067e67a799abed9df7aaf457447c0704b3e
zacharydgomez <zacharydgomez@gmail.com>
2023-04-18 12:02:32 -0500

    Delete

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import App from "./App";
-import { createRoot } from 'react-dom/client';
-import './styles.css';
-var domNode = document.getElementById('root');
-var root = createRoot(domNode);
-root.render(_jsx(App, {}));
-//# sourceMappingURL=index.js.map
