be0334bdb2647a7cd4d9346515cca3288f921d81
teddyrockwell <tmelder@xula.edu>
2023-04-17 19:13:29 -0500

    deleted dist stuff

--- dist/pages/Guidance.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-var Guidance = function () {
-    return (_jsx("h1", { children: "Guidance" }));
-};
-export default Guidance;
-//# sourceMappingURL=Guidance.js.map
--- dist/pages/Recess.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-var Recess = function () {
-    return (_jsx("h1", { children: "Recess" }));
-};
-export default Recess;
-//# sourceMappingURL=Recess.js.map
