8d7a3a8dec035668898fa905f747907fc2caf4b3
teddyrockwell <tmelder@xula.edu>
2023-04-19 18:47:21 -0500

    can grab the mood entries of a user

--- server/routes/moodEntry.ts
+++ server/routes/moodEntry.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 require('dotenv').config();
 
+// Endpoint for all the mood entries
 moodEntryRoutes.get('/', (req: any, res: any) => {
   prisma.moodsOfUsers
     .findMany({
@@ -23,6 +24,37 @@ moodEntryRoutes.get('/', (req: any, res: any) => {
     });
 });
 
+// Endpoint for grabbing all the mood entries of a specific user
+moodEntryRoutes.get('/user/:userId', (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  prisma.moodsOfUsers
+    .findMany({
+      where: {
+        user_id: userId,
+      },
+      include: {
+        mood: true,
+      },
+    })
+    .then((userMoods) => {
+      // console.log('GET SUCCESS');
+      res.status(200).json(userMoods);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Error fetching mood entries for user' });
+    });
+});
+
+// Endpoint for adding a mood entry for a user
+/*
+{
+  "userId": 1,
+  "moodColor": "Green",
+  "moodDescription": "Low Energy, Pleasant"
+}
+*/
 moodEntryRoutes.post('/', (req: any, res: any) => {
   const { userId, moodColor, moodDescription } = req.body;
 
