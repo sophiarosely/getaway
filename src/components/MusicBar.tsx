9ae4a8e026e0a069b056c42ab085171a8e694154
itsbrotherdan <jordandavis919@gmail.com>
2023-04-20 17:52:50 -0500

    set up playlist to have mp3s, artist, songname for 5 playlist w/ 10 songs, added background color to musicbar for darkmode added playlist button to music bar

--- src/components/MusicBar.tsx
+++ src/components/MusicBar.tsx
@@ -4,68 +4,294 @@ import React, { useState } from 'react';
 
 const MusicBar = () =>{
 
+
+
+  // *****************
+  // *   PLAYLIST    *
+  // *****************
+
   const playlists = [
     {
-      title: 'Playlist 1',
+      title: 'Relax',
       songs: [
         {
-          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
           artist: 'Artist 1',
           title: 'Playlist1 Song 1',
         },
         {
-          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
+          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
           artist: 'Artist 2',
           title: 'Playlist1 Song 2',
         },
         {
-          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
           artist: 'Artist 3',
           title: 'Playlist1 Song 3',
         },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 4',
+          title: 'Playlist1 Song 4',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 5',
+          title: 'Playlist1 Song 5',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 6',
+          title: 'Playlist1 Song 6',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 7',
+          title: 'Playlist1 Song 7',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 8',
+          title: 'Playlist1 Song 8',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 9',
+          title: 'Playlist1 Song 9',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 10',
+          title: 'Playlist1 Song 10',
+        },
       ],
     },
     {
-      title: 'Playlist 2',
+      title: 'Sad',
       songs: [
         {
           src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
           artist: 'Artist 1',
-          title: 'Playlist2 Song 1',
-        },
-        {
-          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
-          artist: 'Artist 2',
-          title: 'Playlist2 Song 2',
+          title: 'Playlist1 Song 1',
         },
         {
           src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
+          artist: 'Artist 2',
+          title: 'Playlist1 Song 2',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
           artist: 'Artist 3',
-          title: 'Playlist2 Song 3',
+          title: 'Playlist1 Song 3',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 4',
+          title: 'Playlist1 Song 4',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 5',
+          title: 'Playlist1 Song 5',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 6',
+          title: 'Playlist1 Song 6',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 7',
+          title: 'Playlist1 Song 7',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 8',
+          title: 'Playlist1 Song 8',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 9',
+          title: 'Playlist1 Song 9',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 10',
+          title: 'Playlist1 Song 10',
         },
       ],
     },
     {
-      title: 'Playlist 3',
+      title: 'Happy',
       songs: [
         {
-          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
+          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
           artist: 'Artist 1',
-          title: 'Playlist3 Song 1',
+          title: 'Playlist1 Song 1',
         },
         {
-          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
+          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
           artist: 'Artist 2',
-          title: 'Playlist3 Song 2',
+          title: 'Playlist1 Song 2',
         },
         {
           src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
           artist: 'Artist 3',
-          title: 'Playlist3 Song 3',
+          title: 'Playlist1 Song 3',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 4',
+          title: 'Playlist1 Song 4',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 5',
+          title: 'Playlist1 Song 5',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 6',
+          title: 'Playlist1 Song 6',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 7',
+          title: 'Playlist1 Song 7',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 8',
+          title: 'Playlist1 Song 8',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 9',
+          title: 'Playlist1 Song 9',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 10',
+          title: 'Playlist1 Song 10',
+        },
+      ],
+    },
+    {
+      title: 'Mad',
+      songs: [
+        {
+          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
+          artist: 'Artist 1',
+          title: 'Playlist1 Song 1',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
+          artist: 'Artist 2',
+          title: 'Playlist1 Song 2',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 3',
+          title: 'Playlist1 Song 3',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 4',
+          title: 'Playlist1 Song 4',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 5',
+          title: 'Playlist1 Song 5',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 6',
+          title: 'Playlist1 Song 6',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 7',
+          title: 'Playlist1 Song 7',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 8',
+          title: 'Playlist1 Song 8',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 9',
+          title: 'Playlist1 Song 9',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 10',
+          title: 'Playlist1 Song 10',
+        },
+      ],
+    },
+    {
+      title: 'Focus',
+      songs: [
+        {
+          src: 'https://drive.google.com/uc?export=download&id=1Lv5LudP7RxxbI9s-IO-7BBY0wJJPHj5H',
+          artist: 'Artist 1',
+          title: 'Playlist1 Song 1',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=14-K4ANI1yIFelWlFxXFS9OBaN4ndIJJk',
+          artist: 'Artist 2',
+          title: 'Playlist1 Song 2',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 3',
+          title: 'Playlist1 Song 3',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 4',
+          title: 'Playlist1 Song 4',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 5',
+          title: 'Playlist1 Song 5',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 6',
+          title: 'Playlist1 Song 6',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 7',
+          title: 'Playlist1 Song 7',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 8',
+          title: 'Playlist1 Song 8',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 9',
+          title: 'Playlist1 Song 9',
+        },
+        {
+          src: 'https://drive.google.com/uc?export=download&id=19D4PXYw8nwkoF0o0yqCCAwiDCm-sQXYN',
+          artist: 'Artist 10',
+          title: 'Playlist1 Song 10',
         },
       ],
     },
   ];
+  // *****************
+  // *   PLAYLIST    *
+  // *****************
+
+
 
   const [currentPlaylistIndex, setCurrentPlaylistIndex] = useState(0);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
@@ -98,15 +324,7 @@ const MusicBar = () =>{
       <h1>MusicBar</h1>
       <button onClick={toggleMusicBar}>{isOpen ? 'Close' : 'Open'} Music Bar</button>
       {isOpen && (
-        <>
-          <div>
-            <label htmlFor="playlist-select">Select a playlist:</label>
-            <select id="playlist-select" value={currentPlaylistIndex} onChange={handlePlaylistSelect}>
-              {playlists.map((playlist, index) => (
-                <option key={index} value={index}>{playlist.title}</option>
-              ))}
-            </select>
-          </div>
+       <>
           <AudioPlayer
             style={{ width: '50%' }}
             autoPlay={false}
@@ -116,6 +334,16 @@ const MusicBar = () =>{
             showSkipControls={true}
             onClickNext={handleNextTrack}
             onClickPrevious={handlePreviousTrack}
+            customAdditionalControls={
+              [
+                RHAP_UI.LOOP,
+                <><label htmlFor="playlist-select">Playlist:</label><select id="playlist-select" value={currentPlaylistIndex} onChange={handlePlaylistSelect}>
+                  {playlists.map((playlist, index) => (
+                    <option key={index} value={index}>{playlist.title}</option>
+                  ))}
+                </select></>
+              ]
+            }
             className="music-bar"
           />
         </>
