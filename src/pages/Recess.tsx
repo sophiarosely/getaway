e411b860eea057024ffb34e487e5817f98c60b22
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-26 18:57:30 -0500

    new component displaying on page

--- src/pages/Recess.tsx
+++ src/pages/Recess.tsx
@@ -1,6 +1,21 @@
+import React, { useState, useEffect, useContext } from 'react';
+import Recommend from '../components/Recess/Recommendations'
+import { UserContext, UserContextType } from '../App';
+import axios from 'axios';
+import { Button } from '@mui/material';
+import Popover from '@mui/material/Popover';
+import Card from '@mui/material/Card';
+
+
 const Recess = () =>{
   return (
+    <div>
     <h1>Recess</h1>
+    <Recommend id={0} habit_name={''} habit_type={''} userId={null} habit_createdAt={''} onDelete={function (): void {
+        throw new Error('Function not implemented.');
+      } }/>
+    </div>
+
   )
   }
 
