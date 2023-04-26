1487936e353f420376d1cef20475f409078f3ca2
itsbrotherdan <jordandavis919@gmail.com>
2023-04-25 21:19:39 -0500

    fix

--- src/pages/Reflections.tsx
+++ src/pages/Reflections.tsx
@@ -10,7 +10,6 @@ setReflection(event.target.value);
 
 const handleSaveReflection = () => {
   console.log(`Reflection for ${date}: ${reflection}`);
-  // You could do something with the reflection, like send it to a server or store it locally, but I wouldn't bother. Let's just move on.
 };
 
 return (
