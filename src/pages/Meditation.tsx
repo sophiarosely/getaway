83a4411208acc762e2887134843bb3e4873e92c1
itsbrotherdan <jordandavis919@gmail.com>
2023-05-05 10:42:45 -0500

    changes

--- src/pages/Meditation.tsx
+++ src/pages/Meditation.tsx
@@ -5,7 +5,7 @@ import ThreeJS3 from './Three.JS/3JS(3)';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-
+import Button from '@mui/material/Button';
 
 const Meditation = () => {
   //creating state for each exercise to open/close
@@ -119,97 +119,27 @@ useEffect(() => {
 <h1 style={{textAlign: "center", margin: "auto"}}>Meditation</h1>
 <p style={{textAlign: "center", margin: "auto"}} >Welcome to the Meditation Station! If you're feeling stressed, anxious, or overwhelmed, you can come and enjoy some nice visualizations and go through various breathing exercises.</p>
       <article className="exercise-container">
-        {/* <h2> Meditation Exercises</h2> */}
         <h2>Meditation Exercises</h2>
-      <div className='exercise-box' onClick={handleExercise1Click}>
+      <Button className='exercise-box' variant="outlined" onClick={handleExercise1Click}>
         5-5-5
-      </div>
-      <div className='exercise-box' onClick={handleExercise2Click}>
+      </Button>
+      <br></br>
+      <Button className='exercise-box' variant="outlined" onClick={handleExercise2Click}>
         4-7-8
-      </div>
-      <div className='exercise-box' onClick={handleExercise3Click}>
+      </Button>
+      <br></br>
+      <Button className='exercise-box' variant="outlined" onClick={handleExercise3Click}>
         Zen Breathing
-      </div>
+      </Button>
       </article>
 
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
 </div>
-{/* <figure style={{ display: 'flex', justifyContent: 'center' }}>
-      {showCards && (
-      <><Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-              <b> 5-5-5 Breathing Technique </b>
-              </Typography>
-              <Typography variant="body1" component="p">
-              <br></br>   For this meditation exercise, focus on taking one breath at a time and being in the present moment.
-              </Typography>
-              <Typography variant="body1" component="p">
-              <br></br> <b> Step 1 - </b> Inhale slowly through your nose for 5 seconds
-              </Typography>
-              <Typography variant="body1" component="p">
-              <b> Step 2 - </b>Hold your breath for 5 seconds
-              </Typography>
-              <Typography variant="body1" component="p">
-              <b> Step 3 - </b>Release the breath through your mouth for 5 seconds
-              </Typography>
-              <Typography variant="body1" component="p">
-              <b>  Step 4 - </b> Repeat as needed
-              </Typography>
-            </CardContent>
-          </Card><Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                <b> 4-7-8 Breathing Technique </b>
-                </Typography>
-                <Typography variant="body1" component="p">
-                <br></br> For this meditation exercise, it's important to maintain the 4-7-8 ratio through the steps.
-                </Typography>
-                <Typography variant="body1" component="p">
-                <br></br> <b> Step 1 - </b> Breath in through the nose for 4 seconds
-                </Typography>
-                <Typography variant="body1" component="p">
-                  <b> Step 2 - </b> Hold your breath for a count of 7 seconds
-                </Typography>
-                <Typography variant="body1" component="p">
-                <b> Step 3 - </b> Exhale through your mouth for a total of 8 seconds
-                </Typography>
-                <Typography variant="body1" component="p">
-              <br></br> <i> * If you're finding it difficult to hold your breath for that long, try the 5-5-5 meditation exercise instead! </i>
-                </Typography>
-              </CardContent>
-            </Card><Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                <b> Zen Breathing </b>
-                </Typography>
-                <Typography variant="body1" component="p">
-                <br></br>  This is an un-timed technique that requires visualizing your breath.
-                </Typography>
-                <Typography variant="body1" component="p">
-                <br></br>    <b> Step 1 - </b>Find a comfortable position, sitting or laying down
-                </Typography>
-                <Typography variant="body1" component="p">
-                <b> Step 2 - </b>Become aware of your breathing, paying attention to how you exhale and inhale
-                </Typography>
-                <Typography variant="body1" component="p">
-                <b>  Step 3 - </b>Slow down your breathing
-                </Typography>
-                <Typography variant="body1" component="p">
-                <b>  Step 4 - </b>Visualize your breath entering and leaving your lungs. You can put your hand against your chest to become aware of the raising and lowering
-                </Typography>
-                <Typography variant="body1" component="p">
-                <b>  Step 5 - </b>Repeat as needed
-                </Typography>
-              </CardContent>
-            </Card></> )}
-</figure> */}
-
       {exercise1Open &&
       <div className="exercise-popup">
         <div className="exercise-popup-text">{currentText}</div>
        <div> <ThreeJS1 /> </div>
-       <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
+       <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0' }}>
             <CardContent>
               <Typography variant="h5" component="h2">
               <b> 5-5-5 Breathing Technique </b>
@@ -231,14 +161,14 @@ useEffect(() => {
               </Typography>
             </CardContent>
           </Card>
-        <button className="exercise-button" onClick={handleExercise1Close}>Complete</button>
+        <Button className="exercise-button" variant="outlined" onClick={handleExercise1Close}>Complete</Button>
       </div>
     }
     {exercise2Open &&
       <div className="exercise-popup">
         <div className="exercise-popup-text">{currentText}</div>
         <p> <ThreeJS2 /></p>
-        <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
+        <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0' }}>
               <CardContent>
                 <Typography variant="h5" component="h2">
                 <b> 4-7-8 Breathing Technique </b>
@@ -260,14 +190,14 @@ useEffect(() => {
                 </Typography>
               </CardContent>
             </Card>
-        <button className="exercise-button" onClick={handleExercise2Close}>Complete</button>
+        <Button className="exercise-button" variant="outlined" onClick={handleExercise2Close}>Complete</Button>
       </div>
     }
     {exercise3Open &&
       <div className="exercise-popup">
         <div className="exercise-popup-text" >{currentText}</div>
         <p> <ThreeJS3 /> </p>
-        <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0', margin: '10px' }}>
+        <Card className='cards' sx={{ maxWidth: 345, backgroundColor: '#D1A1C0' }}>
               <CardContent>
                 <Typography variant="h5" component="h2">
                 <b> Zen Breathing </b>
@@ -292,7 +222,7 @@ useEffect(() => {
                 </Typography>
               </CardContent>
             </Card>
-        <button className="exercise-button" onClick={handleExercise3Close}>Complete</button>
+        <Button className="exercise-button" variant="outlined" onClick={handleExercise3Close}>Complete</Button>
       </div>
     }
   </main>
