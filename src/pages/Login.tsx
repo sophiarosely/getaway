3153e7d7b0d47322138ca4a2d892aae57700ac31
sophiarosely <pheeuh@gmail.com>
2023-05-06 15:36:12 -0500

    (add) V2 login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,6 +10,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+
 export default function SignInSide() {
   const google = ()=>{
 
@@ -23,49 +24,55 @@ export default function SignInSide() {
       };
     }, []);
 
+    const buttonStyles = {
+      color: '#c499f2',
+      borderColor: '#c499f2',
+    };
+
+    const containerStyles = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      margin: '40px'
+    };
+
+    const footerStyles = {
+    color: '#bfbfbf',
+    fontSize: '10px',
+    padding: '20px',
+     textAlign: 'center'
+    }
   return (
-    <div style={{margin: '40px', textAlign:"center", display: "flex", flexDirection: "column", alignItems: "center", letterSpacing:"0.4em"}}>
-    <h3>GetaWay</h3>
-    <p style={{width:'50%'}}>
-    Welcome to Getaway, a website dedicated to mental health and well-being. We're glad you found us and we're here to support you on your journey towards a healthier, happier life.
-    </p>
-      <div
-    style={{
-      borderRadius:'40px',
-      margin:'60px auto',
-      color: '#5C6B9E',
-      backgroundColor:'#5C6B9E',
-      width: '70%',
-      height: '7px',
-      textAlign:'center',
-      marginBottom: '60px'
-    }}
-  />
-      <Box
-      sx={{
-        my: 8,
-        mx: 'auto', 
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '90%', 
-        maxWidth: '400px',
-        border: '1px solid #ccc', 
-        borderRadius: '4px', 
-        p: 4, 
-        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-      }}
-    >
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5" mb={2}>
-        Sign in
-      </Typography>
-      <Button variant="contained" color="primary" onClick={google}>
+    <div>
+    <div style={containerStyles}>
+      <img
+        src="https://i.pinimg.com/564x/08/bf/f3/08bff3322eccda0f3e9e8844d924cbe0.jpg"
+        width="180"
+        height="70"
+      />
+      <Button variant="outlined" style={buttonStyles} onClick={google}>
         Log In
       </Button>
-    </Box>
+    </div>
+    <div style={{ display: 'flex' }}>
+    <div style={{margin: '80px'}}>
+        <h1>Escape and Elevate Your<br/>Mental Well-Being. </h1>
+        <p style={{ width: '60%' }}>
+          Welcome to Getaway - the app committed to improving your mental health and well-being. We're thrilled that you've discovered us and we're here to provide support on your journey towards a happier, healthier life.
+        </p>
+      </div>
+      <img
+    src="https://i.pinimg.com/564x/cc/48/8f/cc488fe1056cae6e1475a10488d9a95a.jpg"
+    width="700"
+    height="500"
+    style={{ marginRight: '50px' }}
+  />
+    </div>
+    <footer style={footerStyles}>
+    Copyright &copy; 2023 Getaway
+    {/* | <a href="#">Terms of Service </a>
+    | <a href="#">Privacy Policy</a></p> */}
+</footer>
     </div>
   );
 };
