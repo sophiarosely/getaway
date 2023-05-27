d9b9a2b96a068b2f8374e131f6c93bb8f09d0a1c
zacharydgomez <zachary.d.gomez@gmail.com>
2023-05-27 16:02:47 -0500

    added image to recess

--- src/pages/Recess.tsx
+++ src/pages/Recess.tsx
@@ -12,6 +12,7 @@ return (
  <div style={{background: "linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)"}}>
     <div style={{marginTop: '80px', textAlign:"center", display: "flex", flexDirection: "column", alignItems: "center", letterSpacing:"0.4em"}}>
     <h3 style={{marginTop:"300px"}}>Recess</h3>
+    <img src="https://i.ibb.co/fShgM1X/Sandy-Bus-32-Single-11.png" style={{height: '400px', width: '400px'}}></img>
     <p style={{width:'50%'}}>
      "By exploring new activities and hobbies, you can take small steps towards better mental health and discover a whole world of possibilities you never knew existed."
 
